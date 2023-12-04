da00294c88611207c4e18744dccfb70e53224e6a
MacBook <macbook@MacBooks-MacBook-Pro.local>
2023-12-04 16:47:09 +0600

    order place

--- app/Http/Controllers/Frontend/CartController.php
+++ app/Http/Controllers/Frontend/CartController.php
@@ -70,4 +70,9 @@ class CartController extends Controller
 
         return view('frontend.pages.cart');
     }
+
+    public function checkout()
+    {
+        return view('frontend.pages.checkout');
+    }
 }
--- app/Http/Controllers/Frontend/OrderController.php
+++ app/Http/Controllers/Frontend/OrderController.php
@@ -4,21 +4,50 @@ namespace App\Http\Controllers\Frontend;
 
 use App\Http\Controllers\Controller;
 use App\Models\Order;
+use App\Models\OrderDetails;
 use Illuminate\Http\Request;
 
 class OrderController extends Controller
 {
-    public function buyNow($productId)
+    public function orderPlace(Request $request)
     {
-       //apply/booking/reservation/order/auction_apply
+       
+        //validation here
 
-       Order::create([
+        // dd($request->all());
+
+        $cart=session()->get('vcart');
+        
+        //create order
+        $order=Order::create([
             'user_id'=>auth()->user()->id,
-            'product_id'=>$productId,
-       ]);
+            'status'=>'pending',
+            'total_price'=>array_sum(array_column($cart,'subtotal')),
+            'address'=>$request->address,
+            'receiver_mobile'=>$request->phone_number,
+            'receiver_name'=>$request->name,
+            'receiver_email'=>$request->email_address,
+        ]);
 
-       notify()->success('Order placed');
-       return redirect()->back();
+
+        // dd($cart);
+        //create order details
+        foreach($cart as $key=> $item)
+        {
+            OrderDetails::create([
+                'order_id'=>$order->id,
+                // 'product_id'=>$key,
+                'product_id'=>$item['id'],
+                'quantity'=>$item['quantity'],
+                'subtotal'=>$item['subtotal'],
+            ]);
+        }
+
+        session()->forget('vcart');
+        notify()->success('Order placed success.');
+        return redirect()->back();
+
+
 
     }
 
